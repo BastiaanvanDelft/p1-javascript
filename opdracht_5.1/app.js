3d6e5d598b6e31b1f7cba754e579c1d47fcb67d1
BastiaanvanDelft <9019711@student.zadkine.nl>
2022-09-29 14:14:21 +0200

    5.1

---
+++ opdracht_5.1/app.js
@@ -0,0 +1,7 @@
+let titel1 = document.getElementById("titel1");
+
+
+// document.getElementById("titel1").style.color = "teal";
+titel1.style.color = "teal";
+
+titel1.innerText = "Dit is geen titel meer";
---
+++ temp/app.js
@@ -0,0 +1,24 @@
+// function color_change() {
+//     let title = "title"
+
+//     document.getElementById(title).style.color = "red";
+//     document.getElementById(title).style.fontWeight = "normal";
+//     document.getElementById(title).style.fontSize = "3rem";
+// }
+
+// function color_revert() {
+//     let title = "title"
+
+//     document.getElementById(title).style.color = "black";
+// }
+
+// function background_add() {
+//     let liepaja = "liepaja"
+
+//     document.getElementById(liepaja).style.backgroundColor = "violet";
+// }
+
+function text_change() {
+    let liepaja = document.getElementById("liepaja")
+    liepaja.innerText = "The text has been changed!"
+}
