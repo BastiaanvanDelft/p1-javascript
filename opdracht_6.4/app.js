0901e7d7e2b5b647731b106bbc02a4a7cb01442a
BastiaanvanDelft <9019711@student.zadkine.nl>
2022-10-04 11:45:15 +0200

    6.4

---
+++ opdracht_6.4/app.js
@@ -0,0 +1,27 @@
+function calculator(num1, num2) {
+    let answer1 = num1 + num2;
+    console.log(answer1);
+}
+
+calculator(1,12);
+
+function multiplyByFive(num3) {
+   let answer2 = num3 * 5;
+   console.log(answer2);
+}
+
+multiplyByFive(7);
+
+function checkInput(num4, num5) {
+    let answer3 = (num4 > num5) ? num4 : num5;
+    console.log(answer3);
+}
+
+checkInput(11,20);
+
+function calculateMinutes(num6) {
+    let answer4 = num6 / 60;
+    console.log(answer4);
+}
+
+calculateMinutes(3600);
