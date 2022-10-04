248ade7e0fabcd21f35644af966b8a5fab171aac
BastiaanvanDelft <9019711@student.zadkine.nl>
2022-10-04 12:04:07 +0200

    7.2 upd

--- opdracht_7.1/app.js
+++ opdracht_7.1/app.js
@@ -1,6 +1,6 @@
 const budget = 100;
 let priceprompt =  prompt("Hoeveel kost het product dat je wilt kopen?");
-// const product = priceprompt;
+// const product = 60;
 
 if (budget >= priceprompt) {
     document.getElementById("prijs").innerText = "U heeft genoeg geld!"
